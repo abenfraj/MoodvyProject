1931765196e45f31c6e4404df80976a6ce86540d
Youcef <you.neuffdeuxx@gmail.com>
2022-03-07 10:12:50 +0100

    lien

--- MoodvyApp/App.js
+++ MoodvyApp/App.js
@@ -1,15 +1,12 @@
-import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator></Stack.Navigator>
-    </NavigationContainer>
+"rayan"
   );
 };
-
+// https://prod.liveshare.vsengsaas.visualstudio.com/join?CFA60F1244508BFD006E681A6B706C20DF94
 const styles = StyleSheet.create({
   container: {
     flex: 1,
